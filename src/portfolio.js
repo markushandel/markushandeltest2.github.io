cfaf46602b64e4a71b751b168577359ee29adcde
Markus Handel <123426241+mhandels2023@users.noreply.github.com>
2023-09-02 14:15:21 -0700

    command change

--- src/portfolio.js
+++ src/portfolio.js
@@ -245,7 +245,7 @@ const achievementSection = {
     {
       title: "Google Code-In Finalist",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+        "First Pakistan to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
       image: require("./assets/images/codeInLogo.webp"),
       imageAlt: "Google Code-In Logo",
       footerLink: [
